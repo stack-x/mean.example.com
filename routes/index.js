8b5aa84880da9780e44eb7957dc9cc717794d48b
Jason Snider <jason@jasonsnider.com>
2018-01-10 23:47:35 -0600

    Created a vanilla SPA for users.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', name: 'Jason'});
 });
 
-/* GET home page. */
 router.get('/view', function(req, res, next) {
   res.render('view', { title: 'Express', name: 'Jason'});
 });
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', {title:'Hello World'});
-});
-
-router.get('/create', function(req, res, next) {
-  res.render('users/create');
+  res.render('users/index', {title:'User Management'});
 });
 
 module.exports = router;
