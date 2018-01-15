eca535b3297012cba9c659aac02b1fe21d9829f5
Jason Snider <root@jasonsnider>
2018-01-15 11:50:08 -0600

    Added PM2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Jason'});
+  res.render('index', { title: 'Running with PM2 Again 3', name: 'Jason'});
 });
+
 
 module.exports = router;
