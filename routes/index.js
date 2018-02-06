c708ccc9542c661fb7d62bce6c0bcff9ef6ed2da
Jason Snider <root@jasonsnider>
2018-02-06 13:19:32 -0600

    Test logic pm2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Running with PM2 Again 3', name: 'Jason'});
+  res.render('index', { title: 'Hello World', name: 'Jason'});
+});
+
+//crash the server for pm2 testing
+router.get('/exit', function(req, res, next){
+    process.exit(1);
 });
 
 
